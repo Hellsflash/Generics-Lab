1ca0d00643c1321fa3bbcb1400d0c6d5633eac47
Mario Slavov <hellsflash@gmail.com>
2017-07-23 13:20:56 +0300

    Add project files.

---
+++ Scale/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        var left = int.Parse(Console.ReadLine());
+        var right = int.Parse(Console.ReadLine());
+
+        Scale<int> scale = new Scale<int>(left, right);
+
+        Console.WriteLine(scale.GetHavier());
+    }
+}
